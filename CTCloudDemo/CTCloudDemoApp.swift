54c92733f346326276665814b3ead6ae81e5eae2
Yevhen Mekhedov <yevhen.mekhedov@customertimes.com>
2023-04-19 13:56:32 +0300

    initial commit

---
+++ CTCloudDemo/CTCloudDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CTCloudDemoApp.swift
+//  CTCloudDemo
+//
+//  Created by Yevhen Mekhedov on 19.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CTCloudDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
